15dbccee5a3e80187373600010a2fad12cd05db6
Rookoe-haoran <bababylon88@gmail.com>
2021-09-18 20:01:25 +0800

    fix: 加入localStorage解决刷新页面后theme恢复为light

--- src/utils/Hooks/useDarkMode.ts
+++ src/utils/Hooks/useDarkMode.ts
@@ -3,14 +3,16 @@ import * as React from 'react';
 
 const useDarkMode = (): React.Dispatch<React.SetStateAction<string>>[] => {
 
-  const [theme, setTheme] = React.useState<any>('light');
+  const [theme, setTheme] = React.useState<any>(localStorage.theme || 'light');
 
   const colorTheme = theme === 'light' ? 'dark' : 'light';
 
   React.useEffect(() => {
     const root = window.document.documentElement;
+    localStorage.theme = theme;
     root.classList.remove(colorTheme);
     root.classList.add(theme);
+
   }, [theme]);
 
   return [theme, setTheme];
